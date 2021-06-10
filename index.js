21a45bdcb819acf225ee2cde8ff558ecfa860040
Elison Liu <elisonliu97@gmail.com>
2021-06-10 10:53:29 -0400

    created questions and inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const question = ["What is your project title?: ", 
+"Enter your description: ", 
+// "Enter your table of contents: ", 
+"Enter your installation instructions: ", 
+"Enter your usage information: ",
+"Enter your contribution guidelines: ",
+"Enter your test instructions: ",
+"What is the license for the application: ",
+"Enter your Github username: ",
+"Enter your email: "];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log("File Generated"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: question[0],
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: question[1],
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: question[2],
+                name: 'install',
+            },
+            {
+                type: 'input',
+                message: question[3],
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: question[4],
+                name: 'contribution',
+            },
+            {
+                type: 'input',
+                message: question[5],
+                name: 'test',
+            },
+            {
+                type: 'list',
+                message: question[6],
+                name: 'license',
+                choices: ["None", "APL 2.0", "GNU GPL v3", "MIT", "ISC"]
+            },
+            {
+                type: 'input',
+                message: question[7],
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: question[8],
+                name: 'email',
+            },
+        ])
+        .then((response) => {
+            console.log(generateMarkdown(response));
+            writeToFile("README.md", generateMarkdown(response))
+        })
+
+}
 
 // Function call to initialize app
 init();
